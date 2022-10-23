f47a0a0f7b9de7f69ce3b587cde284a2589c60fc
Louis Leung <louizleung@gmail.com>
2022-10-23 18:36:43 +0800

    battleLogic

--- public/battlefield/battlefield.js
+++ public/battlefield/battlefield.js
@@ -26,8 +26,10 @@ let skillList = {
 };
 
 let mudraChecklist = [];
-
+let skillPatternArray_for_attack_animation = ["1"];
 // window.onload = () => {};
+let currentSkill = "";
+let playerIsAttack = false;
 
 async function getMission() {
   let res = await fetch(`/getMission?missionId=${missionId}`);
@@ -98,53 +100,61 @@ async function showSkills() {
     }
     node.addEventListener("click", (event) => {
       console.log(userSkill.skill_name);
+      currentSkill = userSkill.skill_name;
       if (typeof practiceDialog.showModal === "function") {
         practiceDialog.style.display = "flex";
       }
-      // let count = 0;
-      // let stopper = false;
+
       mudraChecklist = [];
-      // while (stopper == false) {
+      skillPatternArray_for_attack_animation = [];
+
       for (let skill in skillList) {
-        console.log(
-          "skill.name: ",
-          skillList[skill].name,
-          "userSkill.skill_name: ",
-          userSkill.skill_name
-        );
         if (userSkill.skill_name == skillList[skill].name) {
-          console.log("THE SAME!!");
+          // console.log("THE SAME!!");
           for (let mudra of skillList[skill].mudra) {
             mudraChecklist.push(mudra);
+            skillPatternArray_for_attack_animation.push(mudra);
             console.log("mudraChecklist: ", mudraChecklist);
+            console.log(
+              "skillPatternArray_for_attack_animation",
+              skillPatternArray_for_attack_animation
+            );
           }
-          // mudraChecklist.push(...skillList[`skill${z}`].mudra);
         }
-        // count++;
-        // if (count == skillList[skills].mudra.length) {
-        //   stopper = true;
-        // }
       }
-      // }
     });
     z++;
   }
 }
 ///for battle logic
 
-async function npcModal() {
+async function battleLogic() {
   let res = await fetch(`/npcSkills?missionId=${missionId}`);
   let npc = await res.json();
 
   let playerRes = await fetch(`/getPlayerModal`);
   let player = await playerRes.json();
 
+  let player_1_SkillRes = await fetch("/showSkills");
+  let player_1_Skill = await player_1_SkillRes.json();
+
   console.log("NPC:", npc);
-  console.log("player", player);
+  console.log("player 1 data", player);
+  console.log("player 1 skills", player_1_Skill);
 
   let playerHp = player[0].hp;
+  let playerSkill_1 = player_1_Skill[0];
+  let playerSkill_2 = player_1_Skill[1];
+  let playerSkill_3 = player_1_Skill[2];
+  let playerSkill_4 = player_1_Skill[3];
+  let player_skill_damage;
 
   player2.style.backgroundImage = `url(${npc[0].character_image})`;
+
+  // let playerSkill_1 = player_1_Skill[0];
+  // let playerSkill_1 = player_1_Skill[0];
+  // let playerSkill_1 = player_1_Skill[0];
+  // let playerSkill_1 = player_1_Skill[0];
 
   let npcSkill = npc[0].skill_name;
   let npcDamage = npc[0].skill_damage;
@@ -153,42 +163,73 @@ async function npcModal() {
   let npc_id = npc[0].skill_id;
   //console.log("halo:", npcSkill, npcDamage);
 
-  function npcAttack() {
-    let skillMotion = document.createElement("div");
+  function playerAttack() {
+    if (playerIsAttack) {
+      for (let skill of player_1_Skill) {
+        if (currentSkill == skill.skill_name) {
+          player_skill_damage = skill.skill_damage;
+        }
+      }
+      //console.log("skill damage is :", player_skill_damage);
+      let skillMotion = document.createElement("div");
+      characterContainer.insertBefore(skillMotion, player2);
+      skillMotion.classList.add("player1_skillMotion");
+      skillMotion.style.backgroundImage = `url(../skills_image/${currentSkill}.png)`;
+      setTimeout(() => {
+        skillMotion.remove();
+      }, 1000);
+      playerIsAttack = false;
+      console.log(playerIsAttack);
 
+      if (npcHp != 0 && npcHp > 0) {
+        npcHp -= player_skill_damage;
+      } else if (npcHp <= 0) {
+        Swal.fire("成功通關").then(() => {
+          window.location = `../battlefield/battlefield.html?missionId=${
+            +missionId + 1
+          }`;
+        });
+      }
+    }
+  }
+
+  function npcAttack(missionId) {
+    let skillMotion = document.createElement("div");
     characterContainer.insertBefore(skillMotion, player2);
     skillMotion.classList.add("player2_skillMotion");
+    skillMotion.style.backgroundImage = `url(${npc_skill_pic})`;
 
     setTimeout(() => {
       skillMotion.remove();
     }, 1000);
     console.log("play hp left!!!: ", playerHp);
     if (playerHp != 0 && playerHp > 0) {
-      playerHp -= 30;
+      playerHp -= npcDamage;
     } else if (playerHp <= 0) {
       clearInterval(attackLoop);
-      console.log("!!!!!!!!!!!!!!!!!!!!Player is dead!!!!!!!!!!!!!!!!!!!!!!!");
+      Swal.fire({
+        title: "Oops...",
+        text: "You Die!!!",
+      }).then(() => {
+        window.location = "../lobby/lobby.html";
+      });
     }
   }
 
+  let player_attack_loop = setInterval(() => {
+    playerAttack();
+  }, 100);
+
   let attackLoop = setInterval(() => {
-    npcAttack();
-  }, 1000);
+    npcAttack(missionId);
+  }, 20000);
 }
-
-// async function playerModal() {
-//   let res = await fetch(`/getPlayerModal`);
-//   let player = await res.json();
-
-//   console.log("player: ", player);
-// }
 
 showSkills();
 getMission();
-// playerModal();
-npcModal();
+battleLogic();
 
-console.log(skillList);
+//console.log(skillList);
 
 ///for player 出招
 dialogClose.addEventListener("click", () => {
@@ -214,7 +255,7 @@ async function init() {
   document.getElementById("webcam-container").appendChild(webcam.canvas);
 }
 
-// init();
+init();
 
 async function loop() {
   webcam.update(); // update the webcam frame
@@ -235,12 +276,21 @@ async function predict() {
   while (len != mudraChecklist.length && mudraChecklist.length != 0) {
     appendMudraImageDiv(mudraChecklist[0]);
   }
-  if (mudraChecklist.length == 0) {
+  if (
+    mudraChecklist.length == 0 &&
+    skillPatternArray_for_attack_animation.length == 0
+  ) {
     appendMudraImageDiv(9889);
+    practiceDialog.style.display = "none";
+    playerIsAttack = true;
+    skillPatternArray_for_attack_animation.push("1");
   }
   prediction.map((word) => {
+    //console.log(word);
+
     if (word.className == mudraChecklist[0] && word.probability > 0.9) {
       mudraChecklist.shift();
+      skillPatternArray_for_attack_animation.shift();
 
       /*  if (mudraChecklist.length > 0) {
           console.log(`The check list is ${mudraChecklist} now`);
