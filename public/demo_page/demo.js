f1f89d51d7e5a5ff7d2570dd1c4ed0442c4ad031
Louis Leung <louizleung@gmail.com>
2022-10-26 09:50:50 +0800

    never gonna give you up

--- public/demo_page/demo.js
+++ public/demo_page/demo.js
@@ -1,5 +1,11 @@
 let continueBtn = document.querySelector(".continue");
 
+let eye = document.querySelector(".rick");
+
 continueBtn.addEventListener("click", () => {
   window.location = "../cutscenes/cutscenes.html";
+});
+
+eye.addEventListener("click", () => {
+  window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 });
